1a55f4141131da912f1f904527cd88e7aceaaf86
domsellen <domsellen@gmail.com>
2018-09-22 14:52:32 +0200

    Setup

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+export const config = {
+  apiKey: "AIzaSyBx4EtcNSwbpzty5Ozv3Qm5IJMAg0E3ICM",
+    authDomain: "unmask-50759.firebaseapp.com",
+    databaseURL: "https://unmask-50759.firebaseio.com",
+    projectId: "unmask-50759",
+    storageBucket: "unmask-50759.appspot.com",
+    messagingSenderId: "285754494268"
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export const databaseGame = firebase.database().ref('Capulets');
